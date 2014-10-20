dc4cfd2bbf565a595455367dcda5bcaf7d5d95e3
fukuroda <fukuroder@live.jp>
2014-10-21 00:13:45 +0900

    インデント修正

--- sample/BLITSineHardSync_entry.cpp
+++ sample/BLITSineHardSync_entry.cpp
@@ -27,36 +27,34 @@ bool DeinitModule()
 }
 
 namespace MyVst{
-
 	// define factory
 	BEGIN_FACTORY_DEF(
-		"fukuroda",							// vendor
-		"https://github.com/fukuroder",		// url
-		"mailto:fukuroder@live.jp")			// e-mail
+		"fukuroda",                     // vendor
+		"https://github.com/fukuroder", // url
+		"mailto:fukuroder@live.jp")     // e-mail
 
 		// register processor
 		DEF_CLASS2(
-		INLINE_UID_FROM_FUID(BLITSineHardSyncProcessorID),	// processor's GUID
-		PClassInfo::kManyInstances,							// ?
-		kVstAudioEffectClass,								// category
-		"BLIT-based sinewave hardsync",						// plug-in name
-		kDistributable,										// component flag
-		PlugType::kInstrumentSynth,							// sub category
-		"1.0.0.000",										// plug-in version
-		kVstVersionString,									// VSTSDK version
-		BLITSineHardSync_processor::create)					// create method
+		INLINE_UID_FROM_FUID(BLITSineHardSyncProcessorID), // processor's GUID
+		PClassInfo::kManyInstances,                        // ?
+		kVstAudioEffectClass,                              // category
+		"BLIT-based sinewave hardsync",                    // plug-in name
+		kDistributable,                                    // component flag
+		PlugType::kInstrumentSynth,                        // sub category
+		"1.0.0.000",                                       // plug-in version
+		kVstVersionString,                                 // VSTSDK version
+		BLITSineHardSync_processor::create)                // create method
 
 		// register controller
 		DEF_CLASS2(
-		INLINE_UID_FROM_FUID(BLITSineHardSyncControllerID),	// controller's GUID
-		PClassInfo::kManyInstances,							// ?
-		kVstComponentControllerClass,						// category
-		"BLIT-based sinewave hardsync Controller",			// plug-in name
-		0,													// N/A
-		"",													// N/A
-		"1.0.0.000",										// plug-in version
-		kVstVersionString,									// create method
-		BLITSineHardSync_controller::create)				// create method
-
+		INLINE_UID_FROM_FUID(BLITSineHardSyncControllerID), // controller's GUID
+		PClassInfo::kManyInstances,                         // ?
+		kVstComponentControllerClass,                       // category
+		"BLIT-based sinewave hardsync Controller",          // plug-in name
+		0,                                                  // N/A
+		"",                                                 // N/A
+		"1.0.0.000",                                        // plug-in version
+		kVstVersionString,                                  // create method
+		BLITSineHardSync_controller::create)                // create method
 	END_FACTORY
 } // namespace
