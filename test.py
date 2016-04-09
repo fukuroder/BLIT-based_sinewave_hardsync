1f3c1a212b97fa38e2334952af7d3bb131eb6884
fukuroda <fukuroder@live.jp>
2016-04-10 05:43:23 +0900

    サンプルコードをpythonに置き換え

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
-### BLIT-based sinewave hardsync(vst3/VC++2013)
+# -*- coding: utf-8 -*-
 
-![Screenshot](https://raw.github.com/fukuroder/BLIT-based_sinewave_hardsync/master/screenshot.png)
-
-- - -
-
-### Algorithm
-This waveform is constructed with additive synthesis and BLIT synthesis.
-
-![Figure1](https://raw.github.com/fukuroder/BLIT-based_sinewave_hardsync/master/figure1.png)
-
-```python
+#------------------------------------------------------------
 # test.py
+#
+# Copyright (c) 2016, fukuroda (https://github.com/fukuroder)
+# Released under the MIT license
+#------------------------------------------------------------
 
 # import
 import math
@@ -19,10 +14,10 @@ import math
 sample_rate = 44100.0 # sample rate
 
 leak = 0.995          # [0.99, 1.0)
-# used for leaky integrator
+                      # used for leaky integrator
 
 slave = 1.2           # [1.0, 2.0]
-# (slave_freq = master_freq * slave)
+                      # (slave_freq = master_freq * slave)
 
 # set frequency...
 master_freq = 440.0                                           # master frequency
@@ -37,18 +32,12 @@ b3 = -4*math.sin(math.pi*slave)                               # used for BLIT se
 t = 0.5                 # current position
 blit_sum = 0.0          # current value for BLIT section
 for i in range(1000):
-# output
-print b1*math.sin(2*math.pi*1*t) + b2*math.sin(2*math.pi*2*t) + b3*blit_sum
+    # output
+    print b1*math.sin(2*math.pi*1*t) + b2*math.sin(2*math.pi*2*t) + b3*blit_sum
 
-# update position
-t += dt
+    # update position
+    t += dt
 
-# update BLIT
-blit_sum = leak*blit_sum \
-+ math.cos((n+3)*math.pi*t)*math.sin((n-3+1)*math.pi*t)/math.sin(math.pi*t)*dt
-```
-
-- - -
-
-### Implementation by Reaktor
-* [BLIT-based sinewave hardsync](http://www.native-instruments.com/jp/community/reaktor-user-library/entry/show/7639/)
+    # update BLIT
+    blit_sum = leak*blit_sum \
+             + math.cos((n+3)*math.pi*t)*math.sin((n-3+1)*math.pi*t)/math.sin(math.pi*t)*dt
