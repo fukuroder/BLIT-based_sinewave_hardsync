0da834564a0b242190a2fe71c84a96c77062deba
fukuroda <fukuroder@live.jp>
2016-04-30 21:52:31 +0900

    Update

--- test_plot.py
+++ test_plot.py
@@ -23,30 +23,30 @@ slave = 1.2           # [1.0, 2.0]
 # set frequency...
 master_freq = 441.0                                           # master frequency
 
-n = int(sample_rate/2/master_freq)                            # Nyquist limit (round down)
-dt = master_freq/sample_rate                                  # delta t
+n = int(sample_rate/2/master_freq)                            # Nyquist limit
+dt = 2*math.pi*master_freq/sample_rate                        # delta t
 b1 = -2*math.sin(math.pi*slave)/(math.pi*(1+slave)*(1-slave)) # Fourier coefficient for sin(2*PI*1*t)
 b2 = -4*math.sin(math.pi*slave)/(math.pi*(2+slave)*(2-slave)) # Fourier coefficient for sin(2*PI*2*t)
-b3 = -4*math.sin(math.pi*slave)                               # used for BLIT section
+b3 = -2*math.sin(math.pi*slave)/math.pi                       # used for BLIT section
 
 # for each sample...
-t = 0.5                 # current position
+t = -math.pi            # current position
 blit_sum = 0.0          # current value for BLIT section
 v1 = []
 v2 = []
 v3 = []
 for i in range(400):
     # output
-    v1.append(b1*math.sin(2*math.pi*1*t) + b2*math.sin(2*math.pi*2*t))
+    v1.append(b1*math.sin(t) + b2*math.sin(2*t))
     v2.append(b3*blit_sum)
-    v3.append(b1*math.sin(2*math.pi*1*t) + b2*math.sin(2*math.pi*2*t) + b3*blit_sum)
+    v3.append(b1*math.sin(t) + b2*math.sin(2*t) + b3*blit_sum)
 
     # update position
     t += dt
 
     # update BLIT
     blit_sum = leak*blit_sum \
-             + math.cos((n+3)*math.pi*t)*math.sin((n-3+1)*math.pi*t)/math.sin(math.pi*t)*dt
+             + math.cos((n+3)*0.5*t)*math.sin((n-3+1)*0.5*t)/math.sin(0.5*t)*dt
 
 # Additive synthesis section(k=1...2)
 p1 = plt.subplot(131)
