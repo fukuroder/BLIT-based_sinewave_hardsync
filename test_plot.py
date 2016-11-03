1f1450b693195b8f67b084ce9418391607f308f0
fukuroder <fukuroder@live.jp>
2016-11-04 01:09:55 +0900

    効率改善

--- test_plot.py
+++ test_plot.py
@@ -23,11 +23,11 @@ slave = 1.2           # [1.0, 2.0]
 # set frequency...
 master_freq = 441.0                                           # master frequency
 
-n = int(sample_rate/2/master_freq)                            # Nyquist limit
-dt = 2*math.pi*master_freq/sample_rate                        # delta t
-b1 = -2*math.sin(math.pi*slave)/(math.pi*(1+slave)*(1-slave)) # Fourier coefficient for sin(2*PI*1*t)
-b2 = -4*math.sin(math.pi*slave)/(math.pi*(2+slave)*(2-slave)) # Fourier coefficient for sin(2*PI*2*t)
-b3 = -2*math.sin(math.pi*slave)/math.pi                       # used for BLIT section
+n = int(sample_rate/2/master_freq)                                # Nyquist limit
+dt = 2*math.pi*master_freq/sample_rate                            # delta t
+b1 = math.sin(math.pi*slave)/math.pi*(-1/(1+slave)-1/(1-slave)+2) # Fourier coefficient for sin(2*PI*1*t)
+b2 = math.sin(math.pi*slave)/math.pi*(-1/(2+slave)-1/(2-slave)+1) # Fourier coefficient for sin(2*PI*2*t)
+b3 = -math.sin(math.pi*slave)/math.pi                             # used for BLIT section
 
 # for each sample...
 t = -math.pi            # current position
@@ -45,34 +45,33 @@ for i in range(400):
     t += dt
 
     # update BLIT
-    blit_sum = leak*blit_sum \
-             + math.cos((n+3)*0.5*t)*math.sin((n-3+1)*0.5*t)/math.sin(0.5*t)*dt
+    blit_sum = leak*blit_sum + (math.sin((n+0.5)*t)/math.sin(0.5*t)-1.0)*dt
 
 # Additive synthesis section(k=1...2)
 p1 = plt.subplot(131)
 plt.plot(v1)
-plt.ylim([-1.2,1.2])
+plt.ylim([-1.3,1.3])
 plt.grid()
 plt.setp(p1.get_xticklabels(), visible=False)
 plt.setp(p1.get_yticklabels(), visible=False)
-plt.title('Additive synthesis section(k=1...2)', fontsize=10)
+plt.title('Additive synthesis(k=1...2)', fontsize=10)
 
 # BLIT synthesis section(k=3...N)
 p2 = plt.subplot(132)
 plt.plot(v2)
-plt.ylim([-1.2,1.2])
+plt.ylim([-1.3,1.3])
 plt.grid()
 plt.setp(p2.get_xticklabels(), visible=False)
 plt.setp(p2.get_yticklabels(), visible=False)
-plt.title('BLIT synthesis section(k=3...N)', fontsize=10)
+plt.title('BLIT saw', fontsize=10)
 
 # Additive + BLIT
 p3 = plt.subplot(133)
 plt.plot(v3)
-plt.ylim([-1.2,1.2])
+plt.ylim([-1.3,1.3])
 plt.grid()
 plt.setp(p3.get_xticklabels(), visible=False)
 plt.setp(p3.get_yticklabels(), visible=False)
-plt.title('Additive + BLIT', fontsize=10)
+plt.title('Additive + BLIT_saw', fontsize=10)
 
 plt.show()
